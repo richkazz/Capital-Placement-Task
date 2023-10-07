43348931e42dba3000dd791637a07fc18ce8a357
Richkazz <edawarekaro@gmail.com>
2023-10-07 15:48:12 +0100

    Implemented logging with Serilog to record entries both in files and on the console.

--- CapitalPlacementTask/Program.cs
+++ CapitalPlacementTask/Program.cs
@@ -1,23 +1,46 @@
-var builder = WebApplication.CreateBuilder(args);
+using Serilog;
 
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    var logger = new LoggerConfiguration()
+        .WriteTo.Console()
+        .WriteTo.File("log.txt")
+        .CreateLogger();
+    builder.Logging.AddSerilog(logger);
+
+    Log.Logger = logger;
+    Log.Information("Starting up...");
+
+
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
