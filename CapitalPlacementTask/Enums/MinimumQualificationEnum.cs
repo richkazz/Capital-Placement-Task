ba4c8151c040f3007e5edfb9d0dea9f026f7f867
Richkazz <edawarekaro@gmail.com>
2023-10-08 09:10:29 +0100

    Added values to MinimumQualificationEnum and ProgramTypeEnum enums.

--- CapitalPlacementTask/Enums/MinimumQualificationEnum.cs
+++ CapitalPlacementTask/Enums/MinimumQualificationEnum.cs
@@ -2,5 +2,11 @@
 {
     public enum MinimumQualificationEnum
     {
+        HighSchool = 1,
+        Diploma = 2,
+        Bachelor = 3,
+        Master = 4,
+        Doctorate = 5,
+        Other = 6
     }
 }
--- CapitalPlacementTask/Enums/ProgramTypeEnum.cs
+++ CapitalPlacementTask/Enums/ProgramTypeEnum.cs
@@ -2,6 +2,14 @@
 {
     public enum ProgramTypeEnum
     {
-        Internship = 1
+        Internship = 1,
+        Fellowship = 2,
+        Apprenticeship = 3,
+        Training = 4,
+        FullTimeJob = 5,
+        PartTimeJob = 6,
+        CoOp = 7,
+        Volunteer = 8,
+        Other = 9
     }
 }
